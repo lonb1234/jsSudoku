cd7038c12391b8477b616ec3faffbd442eacbe64
Lon <lonbouweriks@gmail.com>
2020-02-20 21:43:34 +0100

    removed the dropdown boxes and made 2(start and reset) buttons semi-functional

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//
-// document.querySelector('.c1').addEventListener("click", function(){ document.querySelector('#b1').innerHTML=('.c1').innerHTML; });
-
+// sudoku clicks
 
 point=0
 document.querySelector('#b1').addEventListener("click", function(){ if (point<9){point++} else{point=1}; document.querySelector('#b1').innerHTML=point; });
+
+
+
+
+// new Game
+// need to add random numbers to it later!
+document.querySelector('.new').addEventListener("click", function(){document.querySelector('.btn-outline-dark').innerHTML='0'});
+
+// reset
+//temporary!, reset to original settings!
+document.querySelector('.reset').addEventListener("click", function(){document.querySelector('.btn-outline-dark').innerHTML='0'});
